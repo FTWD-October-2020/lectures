e0ed4bac7cf4c171d2fbde409781e852bb694663
Tzikas <niko.tzikas@gmail.com>
2020-10-22 17:14:47 -0400

    chees

--- 10-22/script.js
+++ 10-22/script.js
@@ -47,16 +47,39 @@ console.log("HELLO WORLD!!")
 //}
 
 
-//Get all the buttons
+// //Get all the buttons
+// let buttons = document.querySelectorAll('button')
+// for (let button of buttons) {
+//     //For each button add an event listener
+//     button.onclick = function (event) {
+
+//         //Get this text when i click it. 
+//         let fruit = this.innerHTML;
+
+//         //Find the div with id fruit and add the text 
+//         document.querySelector('#fruit').innerText = fruit
+//     }
+// }
+
+
+
+//Got all buttons
 let buttons = document.querySelectorAll('button')
+
 for (let button of buttons) {
-    //For each button add an event listener
+    //Added event listener to each button
     button.onclick = function (event) {
-
-        //Get this text when i click it. 
-        let fruit = this.innerHTML;
-
-        //Find the div with id fruit and add the text 
-        document.querySelector('#fruit').innerText = fruit
+        //When to parent and then down to h2 tag where veggie name was 
+        let veg = this.parentElement.querySelector('h2').innerText
+        console.log(veg)
+        //Added the text and the cheese to div 
+        document.querySelector("#vegetable").innerText = veg
+        document.querySelector("#vegetable").innerHTML += `
+            <div class="myCheese">
+                <img src="cheese.jpg"/>
+            </div>
+        `
     }
 }
+
+
